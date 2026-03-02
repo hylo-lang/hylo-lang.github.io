b1e13307c1eb0b67551250695deabb8b650a583e
Ambrus Tóth <32463042+tothambrus11@users.noreply.github.com>
2026-03-02 20:38:08 +0200

    Update meeting link to Teams

--- src/links.ts
+++ src/links.ts
@@ -1,4 +1,4 @@
 export const slackLink = "https://join.slack.com/t/val-qs97696/shared_invite/zt-1z3dsblrq-y4qXfEE6wr6uMEJSN9uFyg";
 export const compilerRepoLink = "https://github.com/hylo-lang/hylo";
 export const discussionsLink = "https://github.com/orgs/hylo-lang/discussions";
-export const meetingLink = "https://unige.zoom.us/j/66489601602?pwd=z8jMbhAcbxeZBy9js6YTKOIyfSeRrd.1"
+export const meetingLink = "https://teams.microsoft.com/meet/32857354149266?p=w7gAETVhsCc3U09wPm"
