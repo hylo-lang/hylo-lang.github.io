5ab97cd970191d22b22fde3f63befba1ebe49e61
Ambrus Toth <ping@ambrus.dev>
2026-04-30 13:05:04 +0300

    Cleanup

--- src/content/docs/docs/contributing/.sidebar.ts
+++ src/content/docs/docs/contributing/.sidebar.ts
@@ -3,16 +3,18 @@ import {type StarlightUserConfig} from "@astrojs/starlight/types";
 type Sidebar = StarlightUserConfig['sidebar'];
 type ArrayElement<A> = A extends readonly (infer T)[] ? T : never
 
-export const contributingGuideSidebar: ArrayElement<Sidebar> = {
-    label: 'Contributing Guide',
+export const communitySidebar: ArrayElement<Sidebar> = {
+    label: 'Community',
     items: [
-        {label: 'Introduction', slug: 'docs/contributing'},
+        {label: 'Community', slug: 'docs/contributing/community'},
+        {label: 'Contributing Guide', slug: 'docs/contributing'},
         {label: 'Building the Compiler', slug: 'docs/contributing/building-the-compiler'},
         {label: 'Coding Conventions', slug: 'docs/contributing/coding-conventions'},
         {label: 'Research Collaboration', slug: 'docs/contributing/research-collab'},
         {label: 'Editing this Documentation', slug: 'docs/contributing/documentation'},
         {label: 'Diversity and Inclusion', slug: 'docs/contributing/diversity-and-inclusion'},
         {label: 'Code of Conduct', slug: 'docs/contributing/code-of-conduct'},
+        {label: 'Blog', link: '/blog/'},
     ],
 };
 
